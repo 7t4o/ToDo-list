b70b870c5314b259c14803298a32b0061236f770
7t4o <hatemlaptop1@gmail.com>
2024-07-11 06:36:19 +0300

    clean the file

--- main.js
+++ main.js
@@ -3,20 +3,15 @@ let inp = document.getElementById("inp");
 let lst = document.getElementById("list");
 let count = 1;
 
-function getText() {
-    if(inp.value === ""){
-        return false;
-    }else{
-        createTodo(inp.value);
-        inp.value = "";
-    }
-}
+function getText() {inp.value !== "" ? createTodo(inp.value) :"";}
 
 function createTodo(text) {
+    inp.value = "";
     let toDo = document.createElement("div");
     toDo.id = "toDo";
     toDo.setAttribute("count", count+"")
     toDo.appendChild(document.createTextNode(text));
+
     let toDo_btn = document.createElement("i");
     toDo_btn.className = "fa-solid fa-circle-xmark";
     toDo_btn.id = "toDo_btn";
@@ -32,16 +27,11 @@ function deleteTodo() {
     let lst_i = document.querySelectorAll("#toDo i");
     lst_i.forEach(ele => {
         ele.addEventListener('click', function() {
-            const dataId = this.getAttribute('count');
-            const elementToDelete = document.querySelector(`[count="${dataId}"]`);
-            if (elementToDelete) {
-                elementToDelete.remove();
-            }
+            let elementToDelete = document.querySelector(`[count="${this.getAttribute('count')}"]`);
+            elementToDelete ? elementToDelete.remove(): "";
         });
     });
 }
-
-
 
 document.addEventListener('keypress', function(e) {if (e.key === 'Enter') {btn.click();}});
 
